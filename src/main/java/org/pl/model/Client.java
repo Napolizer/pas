3ca3e12d1a82b5382eed188e5149f8388e969f5d
Napolizer <napolizer@op.pl>
2022-10-09 20:47:52 +0200

    Implemented /exceptions and /model classes without TESTS.

---
+++ src/main/java/org/pl/model/Client.java
@@ -0,0 +1,26 @@
+package org.pl.model;
+
+import lombok.Builder;
+import lombok.Data;
+import org.pl.exceptions.ClientException;
+
+@Data
+@Builder
+public class Client {
+    private boolean archive;
+    private double balance;
+    private String firstName;
+    private String lastName;
+    private int personalId;
+    private String phoneNumber;
+    private ClientType clientType;
+    private Address address;
+
+    public double calculateDiscount(int price) throws ClientException {
+        return getClientType().calculateDiscount(price);
+    }
+
+    public void changeBalance(double amount) {
+        setBalance(getBalance() + amount);
+    }
+}
