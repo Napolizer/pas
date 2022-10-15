bbf4bf3c274285909ac6f1ccd6c1efdb192e282c
Rzozef <236533@edu.p.lodz.pl>
2022-10-15 19:55:21 +0200

    fixed repositories tests

--- src/test/java/org/pl/repositories/RepairReposioryTest.java
+++ src/test/java/org/pl/repositories/RepairReposioryTest.java
@@ -2,6 +2,8 @@ package org.pl.repositories;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.pl.exceptions.ClientException;
+import org.pl.exceptions.HardwareException;
 import org.pl.exceptions.RepositoryException;
 import org.pl.model.*;
 
@@ -12,135 +14,159 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.pl.model.Condition.DUSTY;
 
 public class RepairReposioryTest {
-//    Repository<Entity> repository;
-//    Address address;
-//    Client client;
-//    Hardware hardware;
-//    Entity entity;
-//    Entity entity2;
-//    ArrayList<Entity> list;
-//
-//    @BeforeEach
-//    void setUp() {
-//        address = Address.builder()
-//                .street("White")
-//                .number("123")
-//                .city("Lodz")
-//                .build();
-//
-//        client = Client.builder()
-//                .clientType(new Premium())
-//                .address(address)
-//                .balance(300.0)
-//                .firstName("John")
-//                .lastName("Doe")
-//                .personalId(0)
-//                .phoneNumber("123-123-123")
-//                .archive(false)
-//                .build();
-//        hardware = Hardware.builder()
-//                .archive(false)
-//                .hardwareType(new Computer(DUSTY))
-//                .price(100)
-//                .id(1)
-//                .build();
-//        entity = Repair.builder()
-//                .client(client)
-//                .hardware(hardware)
-//                .archive(true)
-//                .id(0)
-//                .build();
-//        entity2 = Repair.builder()
-//                .client(client)
-//                .hardware(hardware)
-//                .archive(false)
-//                .id(1)
-//                .build();
-//        list = new ArrayList<Entity>();
-//        repository = RepairRepository.builder().elements(list).build();
-//    }
-//
-//    @Test
-//    void getElementsTest() {
-//        assertTrue(repository.getElements() != null);
-//        assertEquals(0, repository.getElements().size());
-//    }
-//
-//    @Test
-//    void addTest() {
-//        assertThrows(RepositoryException.class, () -> repository.add(null));
-//        assertEquals(0, repository.getElements().size());
-//        try {
-//            repository.add(entity);
-//            assertEquals(1, repository.getElements().size());
-//            assertTrue(repository.getElements().get(0) instanceof Repair);
-//        } catch (RepositoryException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @Test
-//    void archiviseTest() {
-//        try {
-//            repository.add(entity);
-//            assertThrows(RepositoryException.class, () -> repository.archivise(entity.getID()));
-//            repository.add(entity2);
-//            repository.archivise(entity2.getID());
-//            assertTrue(entity2.isArchive());
-//        } catch (RepositoryException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @Test
-//    void getTest() {
-//        assertThrows(RepositoryException.class, () -> repository.get(-1));
-//        assertThrows(RepositoryException.class, () -> repository.get(entity.getID()));
-//        try {
-//            repository.add(entity);
-//            assertEquals(entity, repository.get(entity.getID()));
-//            assertThrows(RepositoryException.class, () -> repository.get(entity2.getID()));
-//        } catch (RepositoryException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @Test
-//    void getSizeTest() {
-//        try {
-//            assertEquals(0, repository.getSize(true));
-//            assertEquals(0, repository.getSize(false));
-//            repository.add(entity);
-//            assertEquals(0, repository.getSize(true));
-//            assertEquals(1, repository.getSize(false));
-//            repository.add(entity2);
-//            assertEquals(1, repository.getSize(true));
-//            assertEquals(1, repository.getSize(false));
-//        } catch (RepositoryException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @Test
-//    void isArchiveTest() {
-//        try {
-//            repository.add(entity);
-//            assertTrue(repository.isArchive(entity.getID()));
-//            assertThrows(RepositoryException.class, () -> repository.isArchive(entity2.getID()));
-//        } catch (RepositoryException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @Test
-//    void unarchiviseTest() {
-//        try {
-//            repository.add(entity);
-//            repository.unarchivise(entity.getID());
-//            assertFalse(repository.isArchive(entity.getID()));
-//            assertThrows(RepositoryException.class, () -> repository.unarchivise(entity2.getID()));
-//        } catch (RepositoryException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    RepairRepository repository;
+    Address address;
+    Client client;
+    Hardware hardware;
+    Repair repair1;
+    Repair repair2;
+    ArrayList<Repair> list;
+
+    @BeforeEach
+    void setUp() {
+        address = Address.builder()
+                .street("White")
+                .number("123")
+                .city("Lodz")
+                .build();
+
+        client = Client.builder()
+                .clientType(new Premium())
+                .address(address)
+                .balance(300.0)
+                .firstName("John")
+                .lastName("Doe")
+                .personalId(0)
+                .phoneNumber("123-123-123")
+                .archive(false)
+                .build();
+        hardware = Hardware.builder()
+                .archive(false)
+                .hardwareType(new Computer(DUSTY))
+                .price(100)
+                .id(1)
+                .build();
+        repair1 = Repair.builder()
+                .client(client)
+                .hardware(hardware)
+                .archive(true)
+                .id(0)
+                .build();
+        repair2 = Repair.builder()
+                .client(client)
+                .hardware(hardware)
+                .archive(false)
+                .id(1)
+                .build();
+        list = new ArrayList<>();
+        repository = RepairRepository.builder().elements(list).build();
+    }
+
+    @Test
+    void getElementsTest() {
+        assertTrue(repository.getElements() != null);
+        assertEquals(0, repository.getElements().size());
+    }
+
+    @Test
+    void addTest() {
+        assertThrows(RepositoryException.class, () -> repository.add(null));
+        assertEquals(0, repository.getElements().size());
+        try {
+            repository.add(repair1);
+            assertEquals(1, repository.getElements().size());
+            assertTrue(repository.getElements().get(0) != null);
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void archiviseTest() {
+        try {
+            repository.add(repair1);
+            assertThrows(RepositoryException.class, () -> repository.archivise(repair1.getID()));
+            repository.add(repair2);
+            repository.archivise(repair2.getID());
+            assertTrue(repair2.isArchive());
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void getTest() {
+        assertThrows(RepositoryException.class, () -> repository.get(-1));
+        assertThrows(RepositoryException.class, () -> repository.get(repair1.getID()));
+        try {
+            repository.add(repair1);
+            assertEquals(repair1, repository.get(repair1.getID()));
+            assertThrows(RepositoryException.class, () -> repository.get(repair2.getID()));
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void getSizeTest() {
+        try {
+            assertEquals(0, repository.getSize(true));
+            assertEquals(0, repository.getSize(false));
+            repository.add(repair1);
+            assertEquals(0, repository.getSize(true));
+            assertEquals(1, repository.getSize(false));
+            repository.add(repair2);
+            assertEquals(1, repository.getSize(true));
+            assertEquals(1, repository.getSize(false));
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void isArchiveTest() {
+        try {
+            repository.add(repair1);
+            assertTrue(repository.isArchive(repair2.getID()));
+            assertThrows(RepositoryException.class, () -> repository.isArchive(repair2.getID()));
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void unarchiviseTest() {
+        try {
+            repository.add(repair1);
+            repository.unarchivise(repair1.getID());
+            assertFalse(repository.isArchive(repair1.getID()));
+            assertThrows(RepositoryException.class, () -> repository.unarchivise(repair2.getID()));
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void getClientRepairs() {
+        try {
+            assertEquals(0, repository.getClientRepairs(client));
+            repository.add(repair1);
+            assertEquals(1, repository.getClientRepairs(client));
+            repository.add(repair2);
+            assertEquals(2, repository.getClientRepairs(client));
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void repairTest() {
+        try {
+            repository.add(repair1);
+            repository.repair(0);
+            assertTrue(repository.get(0).isArchive());
+        } catch (RepositoryException | HardwareException | ClientException e) {
+            e.printStackTrace();
+        }
+    }
 }
