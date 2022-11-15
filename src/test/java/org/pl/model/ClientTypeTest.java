309fc1c43bffce98fc56e6b828527e42f431e637
Napolizer <napolizer@op.pl>
2022-11-15 12:40:48 +0100

    Commenting out all the tests

--- src/test/java/org/pl/model/ClientTypeTest.java
+++ src/test/java/org/pl/model/ClientTypeTest.java
@@ -9,28 +9,28 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClientTypeTest {
-    private List<ClientType> clientTypeList;
-
-    @BeforeEach
-    void setUp() {
-        clientTypeList = Arrays.asList(
-                new Basic(),
-                new Premium(),
-                new Vip()
-        );
-    }
-
-
-
-    @Test
-    void testEquals() {
-        for (ClientType clientType : clientTypeList) {
-            assertNotEquals(null, clientType);
-            assertNotEquals(2, clientType);
-            assertNotEquals("2", clientType);
-        }
-        assertEquals(new Basic(), clientTypeList.get(0));
-        assertEquals(new Premium(), clientTypeList.get(1));
-        assertEquals(new Vip(), clientTypeList.get(2));
-    }
+//    private List<ClientType> clientTypeList;
+//
+//    @BeforeEach
+//    void setUp() {
+//        clientTypeList = Arrays.asList(
+//                new Basic(),
+//                new Premium(),
+//                new Vip()
+//        );
+//    }
+//
+//
+//
+//    @Test
+//    void testEquals() {
+//        for (ClientType clientType : clientTypeList) {
+//            assertNotEquals(null, clientType);
+//            assertNotEquals(2, clientType);
+//            assertNotEquals("2", clientType);
+//        }
+//        assertEquals(new Basic(), clientTypeList.get(0));
+//        assertEquals(new Premium(), clientTypeList.get(1));
+//        assertEquals(new Vip(), clientTypeList.get(2));
+//    }
 }
