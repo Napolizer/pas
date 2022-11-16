074236de79f148017722929d298fdccada7299d0
236502 Bednarski Kacper <236502@edu.p.lodz.pl>
2022-11-16 21:10:50 +0100

    Repositories created

--- src/main/java/org/pl/repositories/ClientRepository.java
+++ src/main/java/org/pl/repositories/ClientRepository.java
@@ -1,13 +1,45 @@
 package org.pl.repositories;
 
+import jakarta.persistence.EntityManager;
 import lombok.AllArgsConstructor;
+import org.pl.exceptions.RepositoryException;
 import org.pl.model.Client;
 
-import java.util.ArrayList;
+import java.util.UUID;
 
 @AllArgsConstructor
-public class ClientRepository extends Repository<Client> {
-    public ClientRepository(ArrayList<Client> elements) {
-        super(elements);
+public class ClientRepository {
+    private EntityManager entityManager;
+
+    public Client saveClient(Client client) {
+        if (!entityManager.contains(client)) {
+            entityManager.persist(client);
+        } else {
+            entityManager.merge(client);
+        }
+        return client;
+    }
+
+    public Client getClientById(UUID uuid) throws RepositoryException {
+        Client client = entityManager.find(Client.class, uuid);
+        if (client != null) {
+            return client;
+        }
+        throw new RepositoryException(RepositoryException.REPOSITORY_GET_EXCEPTION);
+    }
+
+    public void deleteClient(UUID id) throws RepositoryException {
+        entityManager.getTransaction().begin();
+        Client client = entityManager.find(Client.class, id);
+        if (client != null) {
+            if (!client.isArchive()) {
+                entityManager.merge(client);
+                client.setArchive(true);
+                entityManager.getTransaction().commit();
+                return;
+            }
+            throw new RepositoryException(RepositoryException.REPOSITORY_ARCHIVE_EXCEPTION);
+        }
+        throw new RepositoryException(RepositoryException.REPOSITORY_GET_EXCEPTION);
     }
 }
--- src/main/java/org/pl/repositories/RepairRepository.java
+++ src/main/java/org/pl/repositories/RepairRepository.java
@@ -1,30 +1,69 @@
 package org.pl.repositories;
 
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaUpdate;
 import lombok.AllArgsConstructor;
-import org.pl.exceptions.ClientException;
-import org.pl.exceptions.HardwareException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.pl.exceptions.RepositoryException;
-import org.pl.model.Client;
 import org.pl.model.Repair;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @AllArgsConstructor
-public class RepairRepository extends Repository<Repair>{
+public class RepairRepository {
 
-    public RepairRepository(ArrayList<Repair> elements) {
-        super(elements);
+    private EntityManager entityManager;
+
+    public Repair saveRepair(Repair repair) {
+        if (!entityManager.contains(repair)) {
+            entityManager.persist(repair);
+        } else {
+            entityManager.merge(repair);
+        }
+        return repair;
+    }
+
+    public Repair getRepairById(UUID uuid) throws RepositoryException {
+        Repair repair = entityManager.find(Repair.class, uuid);
+        if (repair != null) {
+            return repair;
+        }
+        throw new RepositoryException(RepositoryException.REPOSITORY_GET_EXCEPTION);
+    }
+
+    public Repair deleteRepair(UUID id) throws RepositoryException {
+        entityManager.getTransaction().begin();
+        Repair repair = entityManager.find(Repair.class, id);
+        if (repair != null) {
+            if (!repair.isArchive()) {
+                entityManager.merge(repair);
+                repair.setArchive(true);
+                entityManager.getTransaction().commit();
+                return repair;
+            }
+            throw new RepositoryException(RepositoryException.REPOSITORY_ARCHIVE_EXCEPTION);
+        }
+        throw new RepositoryException(RepositoryException.REPOSITORY_GET_EXCEPTION);
     }
 
     public List<Repair> getClientRepairs(UUID clientId) {
         List<Repair> repairs = new ArrayList<>();
-        for (int i = 0; i < getElements().size(); i++) {
-            if (elements.get(i).getClient().getId().equals(clientId)) {
-                repairs.add(elements.get(i));
-            }
+        try (Session session = entityManager.unwrap(Session.class)) {
+            CriteriaBuilder criteriaBuilder = (CriteriaBuilder) session.getCriteriaBuilder();
+            CriteriaQuery<Repair> criteriaQuery = criteriaBuilder.createQuery(Repair.class);
+            Root<Repair> root = criteriaQuery.from(Repair.class);
+            criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("client_id"), clientId));
+            Query<Repair> query = session.createQuery((CriteriaUpdate) criteriaQuery);
+            repairs = query.getResultList();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
         return repairs;
     }
