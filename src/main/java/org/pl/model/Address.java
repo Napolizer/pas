acb2ecaefedd771d8c8580c2e9b5555a4212ba26
Napolizer <napolizer@op.pl>
2022-11-16 16:07:54 +0100

    Tests for Address

--- src/main/java/org/pl/model/Address.java
+++ src/main/java/org/pl/model/Address.java
@@ -4,6 +4,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Builder;
 
@@ -17,9 +19,13 @@ public class Address {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private UUID id;
     @NotBlank
+    @Size(min=2)
+    @Pattern(regexp = "^[^0-9]+$")
     private String city;
     @NotBlank
-    private String number;
+    @Size(min=9,max=9)
+    @Pattern(regexp="(^$|[0-9]{9})")
+    private String phoneNumber;
     @NotBlank
     private String street;
 
