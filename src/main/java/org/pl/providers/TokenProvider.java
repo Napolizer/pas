e4f23970940626087977e8982894f8e4d372897d
Napolizer <napolizer@op.pl>
2022-12-29 18:39:15 +0100

    Secret key is retrieved from properties file.

--- src/main/java/org/pl/providers/TokenProvider.java
+++ src/main/java/org/pl/providers/TokenProvider.java
@@ -3,6 +3,8 @@ package org.pl.providers;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.pl.model.Client;
 import org.pl.model.TokenClaims;
 
@@ -10,21 +12,23 @@ import javax.validation.ValidationException;
 import java.util.Date;
 
 public class TokenProvider {
-    private final String SECRET_KEY = "SECRET_KEY";
+    @Inject
+    @ConfigProperty(name="secret_key", defaultValue = "Default secret key")
+    private String secretKey;
     public String generateToken(Client client) {
         long now = System.currentTimeMillis();
         return Jwts.builder()
-                .setSubject("SampleUser")
+                .setSubject(client.getUsername())
                 .claim("username", client.getUsername())
                 .claim("group", client.getClientAccessType())
                 .setIssuedAt(new Date(now))
-                .setExpiration(new Date(now + 10000000))
-                .signWith(SignatureAlgorithm.HS512, SECRET_KEY).compact();
+                .setExpiration(new Date(now + 900000)) // 15 minutes expiration time
+                .signWith(SignatureAlgorithm.HS512, secretKey).compact();
     }
 
     public TokenClaims getTokenClaims(String token) throws ValidationException {
         try {
-            Claims claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+            Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
             String username = claims.get("username").toString();
             String role = claims.get("group").toString();
             return new TokenClaims(username, role);
