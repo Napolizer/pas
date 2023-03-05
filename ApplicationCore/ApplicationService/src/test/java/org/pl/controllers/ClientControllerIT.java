c2c993fd247cc538300eb5f40f7751feacea54ae
Napolizer <napolizer@op.pl>
2023-03-05 21:13:17 +0100

    More client Controller tests.

--- ApplicationCore/ApplicationService/src/test/java/org/pl/controllers/ClientControllerIT.java
+++ ApplicationCore/ApplicationService/src/test/java/org/pl/controllers/ClientControllerIT.java
@@ -8,6 +8,7 @@ import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 import org.microshed.testing.SharedContainerConfig;
 import org.microshed.testing.jupiter.MicroShedTest;
+import org.pl.model.Client;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,10 +19,49 @@ import static org.hamcrest.CoreMatchers.*;
 @MicroShedTest
 @SharedContainerConfig(AppContainerConfig.class)
 public class ClientControllerIT {
+    static Client client;
 
     @BeforeClass
     public static void setUp() {
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+        client = given()
+                .header("Authorization", "Bearer " + retrieveToken())
+                .when()
+                .get("/api/client/username/admin")
+                .then()
+                .extract()
+                .as(Client.class);
+    }
+
+    private static String retrieveToken() {
+        Map<String, Object> credentials = new HashMap<>();
+        credentials.put("username", "admin");
+        credentials.put("password", "password");
+        return given()
+                .contentType(ContentType.JSON)
+                .body(credentials)
+                .when()
+                .post("/api/client/login")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("token", is(instanceOf(String.class)))
+                .extract()
+                .path("token");
+    }
+
+    @Test
+    public void getClientByUsername() {
+        given()
+                .when()
+                .header("Authorization", "Bearer " + retrieveToken())
+                .get("/api/client/username/admin")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("username", is("admin"));
     }
 
     @Test
