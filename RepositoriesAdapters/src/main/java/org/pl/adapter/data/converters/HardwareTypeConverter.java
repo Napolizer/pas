aabc1dc05c6339dc607d901550c1cab9cb61c7db
Napolizer <napolizer@op.pl>
2023-03-05 13:54:48 +0100

    Fixed bug in HardwareTypeConverter.

--- RepositoriesAdapters/src/main/java/org/pl/adapter/data/converters/HardwareTypeConverter.java
+++ RepositoriesAdapters/src/main/java/org/pl/adapter/data/converters/HardwareTypeConverter.java
@@ -11,22 +11,26 @@ public class HardwareTypeConverter {
     @Inject
     private ConditionConverter conditionConverter;
     public HardwareTypeEnt convert(HardwareType hardwareType) {
-        return switch (hardwareType.getType()) {
+        HardwareTypeEnt hardwareTypeEnt = switch (hardwareType.getType()) {
             case "COMPUTER" -> new ComputerEnt(conditionConverter.convert(hardwareType.getCondition()));
             case "CONSOLE" -> new ConsoleEnt(conditionConverter.convert(hardwareType.getCondition()));
             case "PHONE" -> new PhoneEnt(conditionConverter.convert(hardwareType.getCondition()));
             case "MONITOR" -> new MonitorEnt(conditionConverter.convert(hardwareType.getCondition()));
             default -> throw new RuntimeException("Invalid Hardware Type");
         };
+        hardwareTypeEnt.setId(hardwareType.getId());
+        return hardwareTypeEnt;
     }
 
     public HardwareType convert(HardwareTypeEnt hardwareTypeEnt) {
-        return switch (hardwareTypeEnt.getType()) {
+        HardwareType hardwareType = switch (hardwareTypeEnt.getType()) {
             case "COMPUTER" -> new Computer(conditionConverter.convert(hardwareTypeEnt.getCondition()));
             case "CONSOLE" -> new Console(conditionConverter.convert(hardwareTypeEnt.getCondition()));
             case "PHONE" -> new Phone(conditionConverter.convert(hardwareTypeEnt.getCondition()));
             case "MONITOR" -> new Monitor(conditionConverter.convert(hardwareTypeEnt.getCondition()));
             default -> throw new RuntimeException("Invalid Hardware Type");
         };
+        hardwareType.setId(hardwareTypeEnt.getId());
+        return hardwareType;
     }
 }
