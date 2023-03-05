98e95f60c2cd1946cc63c3f61ddf41f6b7b2033f
Napolizer <napolizer@op.pl>
2023-03-05 15:58:39 +0100

    Repaired Hardware Converter (it wasn't working properly for PUT).

--- RepositoriesAdapters/src/main/java/org/pl/adapter/data/converters/HardwareConverter.java
+++ RepositoriesAdapters/src/main/java/org/pl/adapter/data/converters/HardwareConverter.java
@@ -12,21 +12,21 @@ public class HardwareConverter {
     HardwareTypeConverter hardwareTypeConverter;
     public HardwareEnt convert(Hardware hardware) {
         if (hardware == null) return null;
-        return HardwareEnt.builder()
-                .id(hardware.getId())
-                .archive(hardware.isArchive())
-                .price(hardware.getPrice())
-                .hardwareTypeEnt(hardwareTypeConverter.convert(hardware.getHardwareType()))
-                .build();
+        HardwareEnt hardwareEnt = new HardwareEnt();
+        if (hardware.getId() != null) hardwareEnt.setId(hardware.getId());
+        if (hardware.getArchive() != null) hardwareEnt.setArchive(hardware.isArchive());
+        if (hardware.getPrice() != null) hardwareEnt.setPrice(hardware.getPrice());
+        if (hardware.getHardwareType() != null) hardwareEnt.setHardwareTypeEnt(hardwareTypeConverter.convert(hardware.getHardwareType()));
+        return hardwareEnt;
     }
 
     public Hardware convert(HardwareEnt hardwareEnt) {
         if (hardwareEnt == null) return null;
-        return Hardware.builder()
-                .id(hardwareEnt.getId())
-                .archive(hardwareEnt.isArchive())
-                .price(hardwareEnt.getPrice())
-                .hardwareType(hardwareTypeConverter.convert(hardwareEnt.getHardwareTypeEnt()))
-                .build();
+        Hardware hardware = new Hardware();
+        hardware.setId(hardwareEnt.getId());
+        hardware.setArchive(hardwareEnt.isArchive());
+        hardware.setPrice(hardwareEnt.getPrice());
+        hardware.setHardwareType(hardwareTypeConverter.convert(hardwareEnt.getHardwareTypeEnt()));
+        return hardware;
     }
 }
