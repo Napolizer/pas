162b7f79726764aff1021013357e233c025d5d3f
Napolizer <napolizer@op.pl>
2023-03-05 20:37:31 +0100

    Created ClientController login test.

--- ApplicationCore/ApplicationService/src/test/java/org/pl/controllers/ClientControllerIT.java
+++ ApplicationCore/ApplicationService/src/test/java/org/pl/controllers/ClientControllerIT.java
@@ -1,31 +1,31 @@
 package org.pl.controllers;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.microshed.testing.jupiter.MicroShedTest;
 import org.microshed.testing.testcontainers.ApplicationContainer;
 import org.testcontainers.junit.jupiter.Container;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.equalTo;
-
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
 
 @MicroShedTest
-public class HealthControllerIT {
+public class ClientControllerIT {
     @Container
     public static ApplicationContainer container = new ApplicationContainer()
             .withAppContextRoot("/ApplicationService-1.0-SNAPSHOT")
             .withReadinessPath("/ApplicationService-1.0-SNAPSHOT/api/health");
 
-
     @Test
-    public void getAllHardwaresTest() {
+    public void loginPositiveTest() {
         given()
                 .when()
-                .get("/api/health")
+                .header("Content-Type", "application/json")
+                .body("{\"username\":\"admin\",\"password\":\"password\"}")
+                .get("/api/client/login")
                 .then()
                 .assertThat()
                 .statusCode(200)
-                .contentType("text/plain")
-                .body(equalTo("OK"));
+                .contentType("application/json")
+                .body("token", is(instanceOf(String.class)));
     }
 }
