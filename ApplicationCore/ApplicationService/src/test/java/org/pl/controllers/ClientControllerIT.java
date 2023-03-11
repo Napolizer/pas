9e2021ede79b636cd2f65ab93596facabacf485c
Napolizer <napolizer@op.pl>
2023-03-11 19:48:40 +0100

    Added missing tests to ClientControllerIT.

--- ApplicationCore/ApplicationService/src/test/java/org/pl/controllers/ClientControllerIT.java
+++ ApplicationCore/ApplicationService/src/test/java/org/pl/controllers/ClientControllerIT.java
@@ -7,6 +7,7 @@ import org.microshed.testing.SharedContainerConfig;
 import org.microshed.testing.jupiter.MicroShedTest;
 import org.pl.model.*;
 
+import java.net.http.HttpRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,15 +18,25 @@ import static org.hamcrest.Matchers.greaterThan;
 @MicroShedTest
 @SharedContainerConfig(AppContainerConfig.class)
 public class ClientControllerIT {
+    String userId;
+    String userPassword;
     String adminId;
     String adminPassword;
 
     public ClientControllerIT() {
         adminPassword = "password";
+        userPassword = "password";
         adminId = given()
                 .header("Authorization", "Bearer " + retrieveToken())
                 .when()
                 .get("/api/client/username/admin")
+                .then()
+                .extract()
+                .path("id");
+        userId = given()
+                .header("Authorization", "Bearer " + retrieveUserToken())
+                .when()
+                .get("/api/client/username/user")
                 .then()
                 .extract()
                 .path("id");
@@ -35,6 +46,24 @@ public class ClientControllerIT {
         Map<String, Object> credentials = new HashMap<>();
         credentials.put("username", "admin");
         credentials.put("password", adminPassword);
+        return given()
+                .contentType(ContentType.JSON)
+                .body(credentials)
+                .when()
+                .post("/api/client/login")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("token", is(instanceOf(String.class)))
+                .extract()
+                .path("token");
+    }
+
+    private String retrieveUserToken() {
+        Map<String, Object> credentials = new HashMap<>();
+        credentials.put("username", "user");
+        credentials.put("password", userPassword);
         return given()
                 .contentType(ContentType.JSON)
                 .body(credentials)
@@ -392,6 +421,319 @@ public class ClientControllerIT {
     }
 
     @Nested
+    class GetPastRepairs {
+        @Test
+        void getPastRepairsPositiveTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .get("/api/client/id/" + adminId + "/repair/past")
+                    .then()
+                    .assertThat()
+                    .statusCode(200)
+                    .contentType(ContentType.JSON)
+                    .body("size()", is(0));
+        }
+
+        @Test
+        void getPastRepairsMissingIdTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .get("/api/client/id//repair/past")
+                    .then()
+                    .assertThat()
+                    .statusCode(404);
+        }
+
+        @Test
+        void getPastRepairsNegativeIdTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .get("/api/client/id/-1/repair/past")
+                    .then()
+                    .assertThat()
+                    .statusCode(400);
+        }
+
+        @Test
+        void getPastRepairsWrongIdTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .get("/api/client/id/abc/repair/past")
+                    .then()
+                    .assertThat()
+                    .statusCode(400);
+        }
+
+        @Test
+        void getPastRepairsWrongUrlTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .get("/api/client/id/" + adminId + "/repair/past/abc")
+                    .then()
+                    .assertThat()
+                    .statusCode(404);
+        }
+    }
+
+    @Nested
+    class GetPresentRepairsTest {
+        @Test
+        void getPresentRepairsPositiveTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .get("/api/client/id/" + adminId + "/repair/present")
+                    .then()
+                    .assertThat()
+                    .statusCode(200)
+                    .contentType(ContentType.JSON)
+                    .body("size()", is(0));
+        }
+
+        @Test
+        void getPresentRepairsMissingIdTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .get("/api/client/id//repair/present")
+                    .then()
+                    .assertThat()
+                    .statusCode(404);
+        }
+
+        @Test
+        void getPresentRepairsNegativeIdTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .get("/api/client/id/-1/repair/present")
+                    .then()
+                    .assertThat()
+                    .statusCode(400);
+        }
+
+        @Test
+        void getPresentRepairsWrongIdTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .get("/api/client/id/abc/repair/present")
+                    .then()
+                    .assertThat()
+                    .statusCode(400);
+        }
+
+        @Test
+        void getPresentRepairsWrongUrlTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .get("/api/client/id/" + adminId + "/repair/present/abc")
+                    .then()
+                    .assertThat()
+                    .statusCode(404);
+        }
+    }
+
+    @Nested
+    class DeactivateClient {
+        @Test
+        void deactivateClientPositiveTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .body(HttpRequest.BodyPublishers.noBody())
+                    .when()
+                    .put("/api/client/id/" + userId + "/deactivate")
+                    .then()
+                    .assertThat()
+                    .statusCode(200);
+
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .get("/api/client/id/" + userId)
+                    .then()
+                    .assertThat()
+                    .statusCode(200)
+                    .contentType(ContentType.JSON)
+                    .body("archive", is(true));
+
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .body(HttpRequest.BodyPublishers.noBody())
+                    .when()
+                    .put("/api/client/id/" + userId + "/activate")
+                    .then()
+                    .assertThat()
+                    .statusCode(200);
+        }
+
+        @Test
+        void deactivateClientMissingIdTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .put("/api/client/id//deactivate")
+                    .then()
+                    .assertThat()
+                    .statusCode(404);
+        }
+
+        @Test
+        void deactivateClientNegativeIdTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .put("/api/client/id/-1/deactivate")
+                    .then()
+                    .assertThat()
+                    .statusCode(400);
+        }
+
+        @Test
+        void deactivateClientWrongIdTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .put("/api/client/id/abc/deactivate")
+                    .then()
+                    .assertThat()
+                    .statusCode(400);
+        }
+
+        @Test
+        void deactivateClientWrongUrlTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .put("/api/client/id/" + adminId + "/deactivate/abc")
+                    .then()
+                    .assertThat()
+                    .statusCode(404);
+        }
+
+        @Test
+        void deactivateClientAlreadyDeactivatedTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .put("/api/client/id/" + userId + "/deactivate")
+                    .then()
+                    .assertThat()
+                    .statusCode(200);
+
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .put("/api/client/id/" + userId + "/deactivate")
+                    .then()
+                    .assertThat()
+                    .statusCode(400);
+
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .body(HttpRequest.BodyPublishers.noBody())
+                    .when()
+                    .put("/api/client/id/" + userId + "/activate")
+                    .then()
+                    .assertThat()
+                    .statusCode(200);
+        }
+    }
+
+    @Nested
+    class ActivateClient {
+        @Test
+        void activateClientPositiveTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .put("/api/client/id/" + userId + "/deactivate")
+                    .then()
+                    .assertThat()
+                    .statusCode(200);
+
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .put("/api/client/id/" + userId + "/activate")
+                    .then()
+                    .assertThat()
+                    .statusCode(200);
+
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .get("/api/client/id/" + userId)
+                    .then()
+                    .assertThat()
+                    .statusCode(200)
+                    .contentType(ContentType.JSON)
+                    .body("archive", is(false));
+        }
+
+        @Test
+        void activateClientMissingIdTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .put("/api/client/id//activate")
+                    .then()
+                    .assertThat()
+                    .statusCode(404);
+        }
+
+        @Test
+        void activateClientNegativeIdTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .put("/api/client/id/-1/activate")
+                    .then()
+                    .assertThat()
+                    .statusCode(400);
+        }
+
+        @Test
+        void activateClientWrongIdTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .put("/api/client/id/abc/activate")
+                    .then()
+                    .assertThat()
+                    .statusCode(400);
+        }
+
+        @Test
+        void activateClientWrongUrlTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .put("/api/client/id/" + adminId + "/activate/abc")
+                    .then()
+                    .assertThat()
+                    .statusCode(404);
+        }
+
+        @Test
+        void activateClientAlreadyActivatedTest() {
+            given()
+                    .header("Authorization", "Bearer " + retrieveToken())
+                    .when()
+                    .put("/api/client/id/" + userId + "/activate")
+                    .then()
+                    .assertThat()
+                    .statusCode(400);
+        }
+    }
+
+    @Nested
     class Login {
         @Test
         public void loginPositiveTest() {
@@ -469,7 +811,7 @@ public class ClientControllerIT {
         }
 
         @Test
-public void loginWrongUsernameAndPasswordTest() {
+        public void loginWrongUsernameAndPasswordTest() {
             Map<String, Object> credentials = new HashMap<>();
             credentials.put("username", "abc");
             credentials.put("password", "abc");
@@ -542,6 +884,19 @@ public void loginWrongUsernameAndPasswordTest() {
                     .then()
                     .assertThat()
                     .statusCode(200);
+            // Should not be able to login using old password
+            Map<String, Object> credentials = new HashMap<>();
+            credentials.put("username", "admin");
+            credentials.put("password", "password");
+            given()
+                    .contentType(ContentType.JSON)
+                    .body(credentials)
+                    .when()
+                    .post("/api/client/login")
+                    .then()
+                    .assertThat()
+                    .statusCode(401);
+
             adminPassword = "newPassword";
             password.put("newPassword", "password");
             given()
