43497d670d048206094c9f08523ab9dddfd939e4
236533 Gągorowski Piotr <236533@edu.p.lodz.pl>
2023-03-26 17:44:25 +0200

    changes in dependencies

--- SoapAdapters/src/main/java/org/pl/controllers/ClientSoapController.java
+++ SoapAdapters/src/main/java/org/pl/controllers/ClientSoapController.java
@@ -3,6 +3,7 @@ package org.pl.controllers;
 import jakarta.inject.Inject;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
+import org.pl.model.BasicSoap;
 import org.pl.userinterface.client.ReadClientUseCases;
 import org.pl.userinterface.client.WriteClientUseCases;
 
@@ -14,7 +15,7 @@ public class ClientSoapController {
     private ReadClientUseCases readClientUseCases;
 
     @WebMethod
-    public String hello() {
-        return "HelloWorld!";
+    public BasicSoap hello() {
+        return new BasicSoap();
     }
 }
