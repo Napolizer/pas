e0e4f664f807584eaa7370bb55b98418b05f2409
236533 Gągorowski Piotr <236533@edu.p.lodz.pl>
2023-03-26 13:22:44 +0200

    basic ClientSoapController

---
+++ SoapAdapters/src/main/java/org/pl/controllers/ClientSoapController.java
@@ -0,0 +1,20 @@
+package org.pl.controllers;
+
+import jakarta.inject.Inject;
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebService;
+import org.pl.userinterface.client.ReadClientUseCases;
+import org.pl.userinterface.client.WriteClientUseCases;
+
+@WebService(serviceName = "ClientAPI")
+public class ClientSoapController {
+    @Inject
+    private WriteClientUseCases writeClientUseCases;
+    @Inject
+    private ReadClientUseCases readClientUseCases;
+
+    @WebMethod
+    public String hello() {
+        return "HelloWorld!";
+    }
+}
