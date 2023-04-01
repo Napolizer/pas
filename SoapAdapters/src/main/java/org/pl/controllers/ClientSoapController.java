fc7bff1a9012b0667bfb908aa91e2bb3d36596c0
Napolizer <napolizer@op.pl>
2023-04-01 17:39:40 +0200

    Changes in Dependencies.

--- SoapAdapters/src/main/java/org/pl/controllers/ClientSoapController.java
+++ SoapAdapters/src/main/java/org/pl/controllers/ClientSoapController.java
@@ -1,20 +1,26 @@
 package org.pl.controllers;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
+import org.pl.converters.ClientConverter;
 import org.pl.model.BasicSoap;
 import org.pl.userinterface.client.ReadClientUseCases;
 import org.pl.userinterface.client.WriteClientUseCases;
 
 @WebService(serviceName = "ClientSoapController")
-public class ClientSoapController implements ClientSoap {
+public class ClientSoapController {
     @Inject
     private WriteClientUseCases writeClientUseCases;
     @Inject
     private ReadClientUseCases readClientUseCases;
+    @Inject
+    private ClientConverter clientConverter;
 
+    @WebMethod
     public BasicSoap hello() {
+        System.out.println(clientConverter);
         return new BasicSoap();
     }
 }
