120c49ba756c73cef77c2fb70a3510bc86a44b53
Napolizer <napolizer@op.pl>
2023-04-01 18:25:11 +0200

    Implemented Client endpoints.

--- SoapAdapters/src/main/java/org/pl/controllers/ClientSoapController.java
+++ SoapAdapters/src/main/java/org/pl/controllers/ClientSoapController.java
@@ -5,10 +5,13 @@ import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
 import org.pl.converters.ClientConverter;
 import org.pl.model.ClientSoap;
+import org.pl.model.exceptions.ClientException;
+import org.pl.model.exceptions.RepositoryException;
 import org.pl.userinterface.client.ReadClientUseCases;
 import org.pl.userinterface.client.WriteClientUseCases;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @WebService(serviceName = "ClientSoapController")
@@ -21,14 +24,30 @@ public class ClientSoapController {
     private ClientConverter clientConverter;
 
     @WebMethod
-    public List<ClientSoap> allClients() {
-        System.out.println("readClientUseCases");
-        System.out.println(readClientUseCases);
-        System.out.println("clientConverter");
-        System.out.println(clientConverter);
+    public List<ClientSoap> getAllClients() {
         return readClientUseCases.getAllClients()
                 .stream()
                 .map(clientConverter::convert)
                 .collect(Collectors.toList());
+    }
+
+    @WebMethod
+    public ClientSoap getClientById(UUID id) throws RepositoryException {
+        return clientConverter.convert(readClientUseCases.get(id));
+    }
+
+    @WebMethod
+    public ClientSoap createClient(ClientSoap clientSoap) throws RepositoryException, ClientException {
+        return clientConverter.convert(writeClientUseCases.add(clientConverter.convert(clientSoap)));
+    }
+
+    @WebMethod
+    public ClientSoap updateClient(ClientSoap clientSoap) throws RepositoryException {
+        return clientConverter.convert(writeClientUseCases.updateClient(clientSoap.getId(), clientConverter.convert(clientSoap)));
+    }
+
+    @WebMethod
+    public void deleteClient(UUID id) throws RepositoryException {
+        writeClientUseCases.archive(id);
     }
 }
