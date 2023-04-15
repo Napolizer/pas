b825e307fc3eeb661f648e4d596fcee932890355
236533 Gągorowski Piotr <236533@edu.p.lodz.pl>
2023-04-15 12:24:39 +0200

    DomainModel

--- UserModule/UserApplicationCore/UserDomainModel/src/main/java/org/pl/model/User.java
+++ UserModule/UserApplicationCore/UserDomainModel/src/main/java/org/pl/model/User.java
@@ -1,4 +1,29 @@
 package org.pl.model;
 
+import jakarta.json.bind.annotation.JsonbTransient;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private UUID id;
+    private String username;
+    private String password;
+    private Boolean archive;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private UserAccessType userAccessType;
+
+    @JsonbTransient
+    public String getPassword() {
+        return password;
+    }
 }
---
+++ UserModule/UserApplicationCore/UserDomainModel/src/main/java/org/pl/model/UserAccessType.java
@@ -0,0 +1,7 @@
+package org.pl.model;
+
+public enum UserAccessType {
+    ADMINISTRATORS,
+    EMPLOYEES,
+    USERS
+}
