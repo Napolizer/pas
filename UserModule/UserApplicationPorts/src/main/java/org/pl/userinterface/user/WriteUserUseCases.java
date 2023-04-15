07b71babdad1d26dc4103ecdb24027575ef171c5
Bednarooo <236502@edu.p.lodz.pl>
2023-04-15 13:42:47 +0200

    added UserService and UserException

--- UserModule/UserApplicationPorts/src/main/java/org/pl/userinterface/user/WriteUserUseCases.java
+++ UserModule/UserApplicationPorts/src/main/java/org/pl/userinterface/user/WriteUserUseCases.java
@@ -1,11 +1,12 @@
 package org.pl.userinterface.user;
 
 import org.pl.model.User;
+import org.pl.model.exceptions.UserException;
 
 import java.util.UUID;
 
 public interface WriteUserUseCases {
-    User add(User user);
+    User add(User user) throws UserException;
     User archive(UUID uuid);
     User updateUser(UUID uuid, User user);
     User updatePassword(UUID uuid, String newPassword);
