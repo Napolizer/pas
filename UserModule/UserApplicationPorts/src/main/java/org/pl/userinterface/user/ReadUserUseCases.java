341704041c2c2a57ef559d8bc9c71368b3166cc8
236533 Gągorowski Piotr <236533@edu.p.lodz.pl>
2023-04-15 14:11:21 +0200

    additional method in ports

--- UserModule/UserApplicationPorts/src/main/java/org/pl/userinterface/user/ReadUserUseCases.java
+++ UserModule/UserApplicationPorts/src/main/java/org/pl/userinterface/user/ReadUserUseCases.java
@@ -6,7 +6,13 @@ import java.util.List;
 import java.util.UUID;
 
 public interface ReadUserUseCases {
-    User getUser(UUID uuid);
-    User getUserByUsername(String username);
+    User get(UUID id);
+    String getInfo(UUID id);
+    boolean isUserArchive(UUID id);
     List<User> getAllUsers();
+    int getPresentSize();
+    int getArchiveSize();
+    List<User> getAllUsersFilter(String substr);
+    List<User> getUsersByUsername(String username);
+    User getUserByUsername(String username);
 }
