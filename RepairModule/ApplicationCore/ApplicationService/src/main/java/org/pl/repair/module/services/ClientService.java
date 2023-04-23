92d976d4a7495eb2fc0c24dac21b5d6996da2bd6
Bednarooo <236502@edu.p.lodz.pl>
2023-04-23 22:55:01 +0200

    Added microprofile metrics for Repair Module

--- RepairModule/ApplicationCore/ApplicationService/src/main/java/org/pl/repair/module/services/ClientService.java
+++ RepairModule/ApplicationCore/ApplicationService/src/main/java/org/pl/repair/module/services/ClientService.java
@@ -4,6 +4,11 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.eclipse.microprofile.metrics.MetricRegistry;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
+import org.eclipse.microprofile.metrics.annotation.Metric;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.pl.repair.module.model.exceptions.ClientException;
 import org.pl.repair.module.model.exceptions.RepositoryException;
 import org.pl.repair.module.model.Client;
@@ -20,6 +25,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @ApplicationScoped
+@Counted(name = "ClientService_Counter")
 public class ClientService implements WriteHardwareUseCase, ReadClientUseCases {
     @Inject
     private ReadClientPort readClientPort;
@@ -27,70 +33,116 @@ public class ClientService implements WriteHardwareUseCase, ReadClientUseCases {
     @Inject
     private WriteClientPort writeClientPort;
 
+    @Inject
+    @Metric
+    private MetricRegistry metricRegistry;
+
+    @Counted(name = "ClientService.add_Counter")
+    @Timed(name = "ClientService.add_Timer")
     public Client add(Client client) throws RepositoryException, ClientException {
-        if (Objects.equals(client.getFirstName(), ""))
+        if (Objects.equals(client.getFirstName(), "")) {
+            metricRegistry.counter("ClientService.add.ClientFirstNameException_Counter").inc();
             throw new ClientException(ClientException.CLIENT_FIRST_NAME_EXCEPTION);
-        if (Objects.equals(client.getLastName(), ""))
+        }
+        if (Objects.equals(client.getLastName(), "")) {
+            metricRegistry.counter("ClientService.add.ClientLastNameException_Counter").inc();
             throw new ClientException(ClientException.CLIENT_LAST_NAME_EXCEPTION);
-        if (Objects.equals(client.getPhoneNumber(), ""))
+        }
+        if (Objects.equals(client.getPhoneNumber(), "")) {
+            metricRegistry.counter("ClientService.add.ClientPhoneNumberException_Counter").inc();
             throw new ClientException(ClientException.CLIENT_PHONE_NUMBER_EXCEPTION);
-        if (Objects.equals(client.getAddress(), null))
+        }
+        if (Objects.equals(client.getAddress(), null)) {
+            metricRegistry.counter("ClientService.add.ClientAddressException_Counter").inc();
             throw new ClientException(ClientException.CLIENT_ADDRESS_EXCEPTION);
+        }
         return writeClientPort.createClient(client);
     }
 
+    @Counted(name = "Client.get_Counter")
+    @Timed(name = "Client.get_Timer")
     public Client get(UUID id) throws RepositoryException {
         return readClientPort.getClient(id);
     }
 
+    @Timed(name = "Client.getInfo_Timer")
     public String getInfo(UUID id) throws RepositoryException {
         return readClientPort.getClient(id).toString();
     }
 
+    @Counted(name = "ClientService.getClientBalance_Counter")
+    @Timed(name = "ClientService.getClientBalance_Timer")
     public double getClientBalance(UUID id) throws RepositoryException {
         return readClientPort.getClient(id).getBalance();
     }
 
+    @Counted(name = "ClientService.isClientArchive_Counter")
+    @Timed(name = "ClientService.isClientArchive_Timer")
     public boolean isClientArchive(UUID id) throws RepositoryException {
         return readClientPort.getClient(id).getArchive();
     }
 
+    @Counted(name = "ClientService.archive_Counter")
+    @Timed(name = "ClientService.archive_Timer")
     public Client archive(UUID id) throws RepositoryException {
         return writeClientPort.deleteClient(id);
     }
 
+    @Gauge(unit = "number", name = "ClientService.getAllClients_Gauge")
+    @Counted(name = "ClientService.getAllClients_Counter")
+    @Timed(name = "ClientService.getAllClients_Timer")
     public List<Client> getAllClients() {
         return readClientPort.getAllClients();
     }
 
+    @Counted(name = "ClientService.getClientByUsername_Counter")
+    @Timed(name = "ClientService.getClientByUsername_Timer")
     public Client getClientByUsername(String username) throws RepositoryException {
         return readClientPort.getClientByUsername(username);
     }
 
+    @Gauge(unit = "number", name = "ClientService.getPresentSize_Gauge")
+    @Counted(name = "ClientService.getPresentSize_Counter")
+    @Timed(name = "ClientService.getPresentSize_Timer")
     public int getPresentSize() {
         return readClientPort.getClientList(false).size();
     }
 
+    @Gauge(unit = "number", name = "ClientService.getArchiveSize_Gauge")
+    @Counted(name = "ClientService.getArchiveSize_Counter")
+    @Timed(name = "ClientService.getArchiveSize_Timer")
     public int getArchiveSize() {
         return readClientPort.getClientList(true).size();
     }
 
+    @Gauge(unit = "number", name = "ClientService.getAllClientsFilter_Gauge")
+    @Counted(name = "ClientService.getAllClientsFilter_Counter")
+    @Timed(name = "ClientService.getAllClientsFilter_Timer")
     public List<Client> getAllClientsFilter(String substr) {
         return readClientPort.getAllClientsFilter(substr);
     }
 
+    @Gauge(unit = "number", name = "ClientService.getClientsByUsername_Gauge")
+    @Counted(name = "ClientService.getClientsByUsername_Counter")
+    @Timed(name = "ClientService.getClientsByUsername_Timer")
     public List<Client> getClientsByUsername(String username) {
         return readClientPort.getClientsByUsername(username);
     }
 
+    @Counted(name = "ClientService.updateClient_Counter")
+    @Timed(name = "ClientService.updateClient_Timer")
     public Client updateClient(UUID uuid, Client client) throws RepositoryException {
         return writeClientPort.updateClient(uuid, client);
     }
 
+    @Counted(name = "ClientService.unarchive_Counter")
+    @Timed(name = "ClientService.unarchive_Timer")
     public Client dearchive(UUID uuid) throws RepositoryException {
         return writeClientPort.restoreClient(uuid);
     }
 
+    @Counted(name = "ClientService.getClientTypeById_Counter")
+    @Timed(name = "ClientService.getClientTypeById_Timer")
     public ClientType getClientTypeById(UUID uuid) throws RepositoryException {
         List<Client> clients = getAllClients();
         for (Client client : clients) {
@@ -98,6 +150,7 @@ public class ClientService implements WriteHardwareUseCase, ReadClientUseCases {
                 return client.getClientType();
             }
         }
+        metricRegistry.counter("ClientService.getClientTypeById.RepositoryGetException_Counter").inc();
         throw new RepositoryException(RepositoryException.REPOSITORY_GET_EXCEPTION);
     }
 }
--- RepairModule/ApplicationCore/ApplicationService/src/main/java/org/pl/repair/module/services/RepairService.java
+++ RepairModule/ApplicationCore/ApplicationService/src/main/java/org/pl/repair/module/services/RepairService.java
@@ -4,6 +4,11 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.eclipse.microprofile.metrics.MetricRegistry;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
+import org.eclipse.microprofile.metrics.annotation.Metric;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.pl.repair.module.model.exceptions.ClientException;
 import org.pl.repair.module.model.exceptions.HardwareException;
 import org.pl.repair.module.model.exceptions.RepositoryException;
@@ -18,47 +23,78 @@ import java.util.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ApplicationScoped
+@Counted(name = "RepairService_Counter")
 public class RepairService implements ReadRepairUseCases, WriteRepairUseCases {
     @Inject
     private ReadRepairPort readRepairPort;
 
     @Inject
     private WriteRepairPort writeRepairPort;
+
+    @Inject
+    @Metric
+    private MetricRegistry metricRegistry;
+
+    @Counted(name = "RepairService.add_Counter")
+    @Timed(name = "RepairService.add_Timer")
     public Repair add(Repair repair) throws RepositoryException {
         return writeRepairPort.createRepair(repair);
     }
 
+    @Counted(name = "RepairService.get_Counter")
+    @Timed(name = "RepairService.get_Timer")
     public Repair get(UUID id) throws RepositoryException {
         return readRepairPort.getRepair(id);
     }
 
+    @Counted(name = "RepairService.getInfo_Counter")
+    @Timed(name = "RepairService.getInfo_Timer")
     public String getInfo(UUID id) throws RepositoryException {
         return readRepairPort.getRepair(id).toString();
     }
 
+    @Gauge(unit = "number", name = "RepairService.getAllClientRepairs_Gauge")
+    @Counted(name = "RepairService.getAllClientRepairs_Counter")
+    @Timed(name = "RepairService.getAllClientRepairs_Timer")
     public List<Repair> getAllClientRepairs(UUID clientId) throws RepositoryException {
         return readRepairPort.getClientRepairs(clientId);
     }
 
+    @Counted(name = "RepairService.isRepairArchive_Counter")
+    @Timed(name = "RepairService.isRepairArchive_Timer")
     public boolean isRepairArchive(UUID id) throws RepositoryException {
         return readRepairPort.getRepair(id).getArchive();
     }
+
+    @Counted(name = "RepairService.archive_Counter")
+    @Timed(name = "RepairService.archive_Timer")
     public Repair archivize(UUID id) throws RepositoryException {
         return writeRepairPort.deleteRepair(id);
     }
 
+    @Counted(name = "RepairService.repair_Counter")
+    @Timed(name = "RepairService.repair_Timer")
     public Repair repair(UUID id) throws HardwareException, RepositoryException, ClientException {
         return writeRepairPort.repair(id);
     }
 
+    @Gauge(unit = "number", name = "RepairService.getPresentSize_Gauge")
+    @Counted(name = "RepairService.getPresentSize_Counter")
+    @Timed(name = "RepairService.getPresentSize_Timer")
     public int getPresentSize() {
         return readRepairPort.getRepairList(false).size();
     }
 
+    @Gauge(unit = "number", name = "RepairService.getArchiveSize_Gauge")
+    @Counted(name = "RepairService.getArchiveSize_Counter")
+    @Timed(name = "RepairService.getArchiveSize_Timer")
     public int getArchiveSize() {
         return readRepairPort.getRepairList(true).size();
     }
 
+    @Gauge(unit = "number", name = "RepairService.getClientsPastRepairs_Gauge")
+    @Counted(name = "RepairService.getClientsPastRepairs_Counter")
+    @Timed(name = "RepairService.getClientsPastRepairs_Timer")
     public List<Repair> getClientsPastRepairs(UUID uuid) throws RepositoryException {
         List<Repair> pastRepairs = new ArrayList<>();
         List<Repair> repairs = readRepairPort.getClientRepairs(uuid);
@@ -70,6 +106,9 @@ public class RepairService implements ReadRepairUseCases, WriteRepairUseCases {
         return pastRepairs;
     }
 
+    @Gauge(unit = "number", name = "RepairService.getClientsPresentRepairs_Gauge")
+    @Counted(name = "RepairService.getClientsPresentRepairs_Counter")
+    @Timed(name = "RepairService.getClientsPresentRepairs_Timer")
     public List<Repair> getClientsPresentRepairs(UUID uuid) throws RepositoryException {
         List<Repair> presentRepairs = new ArrayList<>();
         List<Repair> repairs = readRepairPort.getClientRepairs(uuid);
@@ -81,10 +120,16 @@ public class RepairService implements ReadRepairUseCases, WriteRepairUseCases {
         return presentRepairs;
     }
 
+    @Gauge(unit = "number", name = "RepairService.getAllRepairs_Gauge")
+    @Counted(name = "RepairService.getAllRepairs_Counter")
+    @Timed(name = "RepairService.getAllRepairs_Timer")
     public List<Repair> getAllRepairs() {
         return readRepairPort.getAllRepairs();
     }
 
+
+    @Counted(name = "RepairService.updateRepair_Counter")
+    @Timed(name = "RepairService.updateRepair_Timer")
     public Repair updateRepair(UUID uuid, Repair repair) throws RepositoryException {
         return writeRepairPort.updateRepair(uuid, repair);
     }
